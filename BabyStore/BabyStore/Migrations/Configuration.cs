f3655353bda985e3020f91f01666a432342cf022
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-09-23 16:38:21 +0100

    Initial Migration

---
+++ BabyStore/BabyStore/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace BabyStore.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BabyStore.DAL.StoreContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BabyStore.DAL.StoreContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}

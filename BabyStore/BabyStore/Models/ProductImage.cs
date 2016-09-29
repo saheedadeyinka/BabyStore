4142201f7749bae102c769b207788bdf09b621c7
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-09-29 11:32:17 +0100

    Adding ProductImage to tha app and update the database

---
+++ BabyStore/BabyStore/Models/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabyStore.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        [Display(Name = "File")]
+        public string FileName { get; set; }
+    }
+}

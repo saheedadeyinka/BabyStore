7dfd9d1a7bea43fef35077c2a67eb04b6d22661f
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-10-04 01:20:40 +0100

    Adding a unique index key to File Name

--- BabyStore/BabyStore/Models/ProductImage.cs
+++ BabyStore/BabyStore/Models/ProductImage.cs
@@ -1,11 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BabyStore.Models
 {
     public class ProductImage
     {
         public int Id { get; set; }
+
         [Display(Name = "File")]
+        [StringLength(100)]
+        [Index(IsUnique = true)]
         public string FileName { get; set; }
     }
 }

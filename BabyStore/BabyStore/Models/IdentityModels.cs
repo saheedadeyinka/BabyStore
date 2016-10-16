e18cf321f2a2c851e2522e1da76b0f4e827db903
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-10-16 15:07:46 +0100

    Adding Extra properties for users

--- BabyStore/BabyStore/Models/IdentityModels.cs
+++ BabyStore/BabyStore/Models/IdentityModels.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -9,6 +11,23 @@ namespace BabyStore.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Birth")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+
+        public Address Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType

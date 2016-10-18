685351bb0a6d112f523393d0e8e362e8c9ede49e
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-10-18 22:22:51 +0100

    Adding a User Admin Controller

---
+++ BabyStore/BabyStore/Controllers/UsersAdminController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BabyStore.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersAdminController : Controller
+    {
+        public UsersAdminController()
+        {
+
+        }
+
+        public UsersAdminController(ApplicationUserManager userManager, ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private ApplicationRoleManager _roleManager;
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+
+        // GET: UsersAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsersAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsersAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersAdmin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersAdmin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersAdmin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

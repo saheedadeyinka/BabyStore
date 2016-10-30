3d0c7cf04a2b09f7e79c39dca803d8f717fad94d
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-10-30 09:51:47 +0100

    Adding Best Sellers to Admin Page

--- BabyStore/BabyStore/Controllers/HomeController.cs
+++ BabyStore/BabyStore/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BabyStore.DAL;
+using BabyStore.ViewModels;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace BabyStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private StoreContext db = new StoreContext();
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var topSellers = (from topProducts in db.OrderLines
+                              where (topProducts.ProductId != null)
+                              group topProducts by topProducts.Product
+                into topGroup
+                              select new BestSellersViewModel
+                              {
+                                  Product = topGroup.Key,
+                                  SalesCount = topGroup.Sum(o => o.Quantity),
+                                  ProductImage =
+                                      topGroup.Key.ProductImageMappings.OrderBy(pim => pim.ImageNumber)
+                                          .FirstOrDefault()
+                                          .ProductImage.FileName
+                              }).OrderByDescending(tg => tg.SalesCount).Take(4);
+
+            return View(await topSellers.ToListAsync());
         }
 
         public ActionResult About()

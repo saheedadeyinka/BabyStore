f65f4afeaa0cf75d2655ac261a947d3060b47ac2
Saheed Adeniji <adeyinkasaheed4success@hotmail.com>
2016-11-02 12:34:02 +0100

    Reformating the Login Page Information

--- BabyStore/BabyStore/App_Start/IdentityConfig.cs
+++ BabyStore/BabyStore/App_Start/IdentityConfig.cs
@@ -8,7 +8,6 @@ using System;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace BabyStore
 {
@@ -130,57 +129,9 @@ namespace BabyStore
 
         private static void InitializeIdentityForEF(ApplicationDbContext context)
         {
-            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
-            const string name = "admin@mvcbabystore.com";
-            const string password = "Adm1n@mvcbabystore.com";
-            const string roleName = "Admin";
+            //var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
 
-            //Create Role Admin if it does not exist
-            var role = roleManager.FindByName(roleName);
-            if (role == null)
-            {
-                role = new IdentityRole(roleName);
-                var roleResult = roleManager.Create(role);
-            }
-
-            var user = userManager.FindByName(name);
-            if (user == null)
-            {
-                user = new ApplicationUser
-                {
-                    UserName = name,
-                    Email = name,
-                    FirstName = "Admin",
-                    LastName = "Admin",
-                    DateOfBirth = new DateTime(2015, 1, 1),
-                    Address = new Address
-                    {
-                        AddressLine1 = "1 Admin Street",
-                        Town = "Town",
-                        Country = "Country",
-                        PostCode = "PostCode"
-                    }
-                };
-                var result = userManager.Create(user, password);
-                result = userManager.SetLockoutEnabled(user.Id, false);
-            }
-
-            //Add user Admin to Role Admin if not already added
-            var rolesForUser = userManager.GetRoles(user.Id);
-            if (!rolesForUser.Contains(role.Name))
-            {
-                var result = userManager.AddToRole(user.Id, role.Name);
-            }
-
-            //Create users role
-            const string userRoleName = "Users";
-            role = roleManager.FindByName(userRoleName);
-            if (role == null)
-            {
-                role = new IdentityRole(userRoleName);
-                var roleResult = roleManager.Create(role);
-            }
         }
     }
 }
